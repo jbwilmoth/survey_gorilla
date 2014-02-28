131e2c4f0fa00e1a278adda5991fd4698d17c172
Joe Wilmoth <jbwilmoth@gmail.com>
2014-02-28 14:52:37 -0600

    reused user authentication

---
+++ app/controllers/session.rb
@@ -0,0 +1,21 @@
+## Sign in
+get '/login' do
+  erb :"user/login"
+end
+
+post '/login' do
+  @user = User.authenticate(params[:email], params[:password])
+  if @user
+    session[:id] = @user.id
+    redirect "/profile/#{@user.id}"
+  else
+    @errors = ["Username and password do not exist."]
+    erb :"user/login"
+  end
+end
+
+## Log out
+get '/logout' do
+  session[:id] = nil 
+  redirect '/'
+end 
