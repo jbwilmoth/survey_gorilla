044308bbad69010997b0fc320809cf3f20f4ed6a
thejessleigh <j.l.unrein@gmail.com>
2014-02-28 17:27:45 -0600

    Ability to add one question to a survey added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
-  has_many :surveys, source: :creator
-  has_many :responses, source: :taker
+  has_many :surveys, foreign_key: 'creator_id'
+  has_many :responses, foreign_key: 'taker_id'
   has_many :questions, through: :surveys
   has_secure_password
 end
